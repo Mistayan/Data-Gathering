00c1b306da9533da57618b62bd718cbe10b1f0b5
Mistayan <helixmastaz@gmail.com>
2022-08-09 12:57:15 +0200

    run_me for demo license added slight upgrade in search patterns no more dependency on parse module.

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ from urllib3.exceptions import MaxRetryError
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import parse
 from webdriver_manager.chrome import ChromeDriverManager
 
 import conf
@@ -24,8 +23,7 @@ from my_async import AsyncRequest, get_content
 
 log = logging.getLogger(__name__)
 mail_pattern = re.compile(r"[\w\d+._-]+\(?@\)?[\w\d+._-]+\.[a-zA-Z]+")
-mailto_pattern = parse.Parser("mailto:{}\"")
-url_parse = parse.Parser("/contact{}\"")
+contact_pattern = re.compile(r"/.?contact+[\w\d+_/-]+")
 
 
 def extract_mail(string):
@@ -35,7 +33,7 @@ def extract_mail(string):
     """
     if string.find("#&") or string.find("%"):  # mail is unicode-encoded or html-encoded
         string = html.unescape(string)
-    if string.find("?"):  # mailto:{} sometimes returns mail?form=...
+    if string.find("?"):
         string = string.split("?")[0]
     if re.match(r" ?[&?'\"\'\\<(){]+.+", html.unescape(string)):  # mail from mailto: {} sometimes returns extra
         string = re.sub(r" ?[&?'/\"\'\\<(){]+.+", "", html.unescape(string))  # remove potential junk after email
@@ -50,34 +48,26 @@ def extract_mail(string):
         return
 
 
-def get_website_contacts(url):
-    mails = []
+def get_website_mails(url):
+    ret = []
     website = AsyncRequest(get_content, url)
     if website.get():
-        mailtos = mailto_pattern.search(website.get())
-        mails.append(mailtos[0].lower()) if mailtos else None
-        contact_url = url_parse.search(website.get())
-        if contact_url and "css" not in contact_url[0]:
-            # task.name is the requested website's url, at which we append /contact***/
-            contact_url = contact_url[0].split("\"") if "\"" in contact_url[0] \
-                else contact_url[0].split("\'") if "\'" in contact_url[0] \
-                else contact_url
-            contact_url = website.name[0] + ('/' if website.name[0][-1] != '/' else '') + "contact" + contact_url[0]
-            contact_page = str(AsyncRequest(get_content, contact_url).get())
+        mails = mail_pattern.findall(website.get())
+        [ret.append(mailto) for mailto in mails] if mails else None
+        contact = contact_pattern.search(website.get())
+        print(contact)
+        if contact and "css" not in contact[0]:
+            # task.name is the requested website's url, at which we append /*contact*/
+            contact = website.name[0] + (contact[0][1:] if website.name[0][-1] == '/' else contact[0])
+            contact_page = str(AsyncRequest(get_content, contact).get())
             if contact_page:
                 try:
-                    sub_mails = mail_pattern.findall(contact_page)
-                    if sub_mails:
-                        [mails.append(extract_mail(mail)) for mail in sub_mails]
+                    mails = mail_pattern.findall(contact_page)
+                    [ret.append(mail) for mail in mails] if mails else None
                 except ValueError:  # No email found via regex. Maybe it is encoded (may return unwanted values)
-                    try:
-                        sub_mailto = mailto_pattern.search(contact_page)
-                        if sub_mailto:
-                            [mails.append(extract_mail(mail)) if mail.lower() not in mails else None for mail in sub_mailto]
-                    except ValueError:
-                        pass
-    log.info(f"{url} => extracted: {mails}")
-    return mails
+                    pass
+    log.info(f"{url} => extracted: {ret}")
+    return ret
 
 
 def extract_website(_id):
@@ -102,10 +92,10 @@ def extract_website(_id):
 # ______________________________________________________________________________________________ #
 if __name__ == '__main__':
     # Attempt to gather json sent when loading the map
-    print("Data-Gathering  Copyright (C) 2022  Mistayan
+    print("""Data-Gathering  Copyright (C) 2022  Mistayan
     This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
     This is free software, and you are welcome to redistribute it
-    under certain conditions; type `show c' for details.")
+    under certain conditions; type `show c' for details.""")
           
     try:
         with open("test_datas", "r") as fp:  # this file is a copy from browser's network analyser
@@ -115,7 +105,7 @@ if __name__ == '__main__':
     except FileNotFoundError:
         exit(print("Did you clone the repository ? It includes test_datas"))
     # Attempt to gather every website displayed
-    results = [frozenset(get_website_contacts(extract_website(_id))) for _id in id_list]
+    results = [frozenset(get_website_mails(extract_website(_id))) for _id in id_list]
     # save findings
     with open("findings", "w", 1024, "utf-8") as fp:
         for line in results:
